c3c1a4d5d1f4473e0235beb3f5804193379d30f9
vwhb王海兵 <haibingwang@trip.com>
2022-08-16 14:15:56 +0800

    update local

--- mongo-allone-init.js
+++ mongo-allone-init.js
@@ -1,19 +1,6 @@
-print('Start #################################################################');
-
-db = db.getSiblingDB('arex_storage_db');
+db = db.getSiblingDB("arex_storage_db");
 db.createUser({
   user: "arex",
   pwd: "iLoveArex",
-  roles: [
-    { role: 'readWrite', db: 'arex_storage_db'}    
-   ]
-})
-
-db = db.getSiblingDB('fltarexmongodb');
-db.createUser({
-  user: "arex",
-  pwd: "iLoveArex",
-  roles: [
-    { role: 'readWrite', db: 'fltarexmongodb' }    
-   ]
-})
+  roles: [{ role: "readWrite", db: "arex_storage_db" }],
+});
