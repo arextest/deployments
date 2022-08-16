e3889426f9c886830cb943792e48d4e926d62e7f
vwhb王海兵 <haibingwang@trip.com>
2022-08-16 16:11:22 +0800

    update version

--- storage.build.sh
+++ storage.build.sh
@@ -2,7 +2,7 @@ cd ../arex-storage
 mvn clean package -DskipTests
 
 cp ../deployments/storage.Dockerfile ./Dockerfile
-docker build -t   arexadmin01/arex-storage-serive:0.1 .
+docker build -t   arexadmin01/arex-storage-serive:0.2 .
 rm -rf ./Dockerfile
 
 cd ..
