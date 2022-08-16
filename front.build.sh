13c958d8ef9d6bd0789eb0d8a9463fc19c5fd338
vwhb王海兵 <haibingwang@trip.com>
2022-08-16 19:12:18 +0800

    update version

--- front.build.sh
+++ front.build.sh
@@ -1,7 +1,7 @@
 cd ../arex-front-end
 
 cp ../deployments/dockerfile/front.Dockerfile ./Dockerfile
-docker build -t arexadmin01/replay-front-end:0.2  .  --build-arg http_proxy=http://ntproxy.qa.nt.ctripcorp.com:8080 --build-arg https_proxy=http://ntproxy.qa.nt.ctripcorp.com:8080 
+docker build -t arexadmin01/replay-front-end:0.1  .  --build-arg http_proxy=http://ntproxy.qa.nt.ctripcorp.com:8080 --build-arg https_proxy=http://ntproxy.qa.nt.ctripcorp.com:8080 
 rm -rf ./Dockerfile
 
 cd ..
--- report.build.sh
+++ report.build.sh
@@ -2,7 +2,7 @@ cd ../arex-report
 mvn clean package -DskipTests
 
 cp ../deployments/dockerfile/report.Dockerfile ./Dockerfile
-docker build -t arexadmin01/arex-report:0.2  .
+docker build -t arexadmin01/arex-report:0.1  .
 rm -rf ./Dockerfile
 
 cd ..
