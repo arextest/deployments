602b9b38ff55da2894f1ca5f9562918b25a7195e
vwhb王海兵 <haibingwang@trip.com>
2022-08-26 16:30:35 +0800

    rollback dockerfile version to 0.2

--- config.build.sh
+++ config.build.sh
@@ -2,7 +2,7 @@ cd ../arex-config
 mvn clean package -DskipTestsa
 
 cp ../deployments/dockerfile/config.Dockerfile ./Dockerfile
-docker build -t arexadmin01/arex-config:0.2.1  .
+docker build -t arexadmin01/arex-config:0.2  .
 rm -rf ./Dockerfile
 
 cd ..
