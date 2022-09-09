b1f2a922a50a157633f762529c59d83d82cb6407
vwhb王海兵 <haibingwang@trip.com>
2022-09-09 16:16:12 +0800

    update shell

--- arex-upload-docker.sh
+++ arex-upload-docker.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 if [ $# -lt 1 ]
 then
-  echo "Input version please! e.g. 0.2.2"
+  echo "Input version please! e.g. 0.2.2............."
   exit
 fi
+
+echo "Tag version: $1"
+docker tag arexadmin01/arex:latest arexadmin01/arex:$1 
+docker tag arexadmin01/arex-report:latest arexadmin01/arex-report:$1  
+docker tag arexadmin01/arex-storage-serive:latest arexadmin01/arex-storage-serive:$1  
+docker tag arexadmin01/arex-config:latest arexadmin01/arex-config:$1  
+docker tag arexadmin01/arex-replay-schedule:latest arexadmin01/arex-replay-schedule:$1 
+
+echo "Push version:$1"
+docker push arexadmin01/arex:$1
+docker push arexadmin01/arex-report:$1
+docker push arexadmin01/arex-storage-serive:$1
+docker push arexadmin01/arex-config:$1
+docker push arexadmin01/arex-replay-schedule:$1
 
 echo "Push latest"
 docker push arexadmin01/arex:latest
@@ -12,21 +26,5 @@ docker push arexadmin01/arex-storage-serive:latest
 docker push arexadmin01/arex-config:latest
 docker push arexadmin01/arex-replay-schedule:latest
 
-if [ "$2" -eq "-origin" ]
-then
-    echo "Tag version: $1"
-    docker tag arexadmin01/arex:latest arexadmin01/arex:$1 
-    docker tag arexadmin01/arex-report:latest arexadmin01/arex-report:$1  
-    docker tag arexadmin01/arex-storage-serive:latest arexadmin01/arex-storage-serive:$1  
-    docker tag arexadmin01/arex-config:latest arexadmin01/arex-config:$1  
-    docker tag arexadmin01/arex-replay-schedule:latest arexadmin01/arex-replay-schedule:$1 
-
-    echo "Push version:$1"
-    docker push arexadmin01/arex:$1
-    docker push arexadmin01/arex-report:$1
-    docker push arexadmin01/arex-storage-serive:$1
-    docker push arexadmin01/arex-config:$1
-    docker push arexadmin01/arex-replay-schedule:$1
-fi
 
 
