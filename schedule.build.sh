9bc7ad32fd72aa6268a26e3db6ddefab7ab57427
vwhb王海兵 <haibingwang@trip.com>
2022-09-09 14:40:47 +0800

    update ver as latest

--- schedule.build.sh
+++ schedule.build.sh
@@ -2,7 +2,7 @@ cd ../arex-replay-schedule
 mvn clean package -DskipTests
 
 cp ../deployments/dockerfile/schedule.Dockerfile ./Dockerfile
-docker build -t  arexadmin01/arex-replay-schedule:0.2.1  .
+docker build -t  arexadmin01/arex-replay-schedule:latest  .
 rm -rf ./Dockerfile
 
 cd ..
