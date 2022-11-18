0e22c968531533af5fd561a9235d9203bbab9039
vwhb王海兵 <haibingwang@trip.com>
2022-11-18 19:47:42 +0800

    fix dockerfile

--- community.build.sh
+++ community.build.sh
@@ -2,13 +2,13 @@ cd ../arex-community-test
 mvn clean package -DskipTests
 
 cp ../deployments/dockerfile/community.Dockerfile ./Dockerfile
-cp ../deployments/agent/arex-agent-0.*.jar ./arex-agent.jar
-cp ../deployments/agent/arex-agent-bootstrap-*.jar ./arex-agent-bootstrap.jar
+cp ../../arex-agent-java/arex-agent-jar/arex-agent-0.1.0.jar ./arex-agent-0.1.0.jar
+cp ../../arex-agent-java/arex-agent-jar/arex-agent-bootstrap-0.1.0.jar ./arex-agent-bootstrap-0.1.0.jar
 
 docker build -t arexadmin01/arex-community-test:0.0.1  .
 
 rm -rf ./Dockerfile
-rm -rf ./arex-agent.jar
-rm -rf ./arex-agent-bootstrap.jar
+rm -rf ./arex-agent-0.1.0.jar
+rm -rf ./arex-agent-bootstrap-0.1.0.jar
 
 cd ..
--- dockerfile/community.Dockerfile
+++ dockerfile/community.Dockerfile
@@ -7,7 +7,8 @@ WORKDIR /usr/local/tomcat/conf
 RUN sed -i 'N;152a\\t<Context path="" docBase="arex-agent-test-0.0.1-SNAPSHOT" reloadable="true" />' server.xml
 
 #RUN set -eux && mkdir -p /usr/arex/agent
-ADD ./arex-agent-*.jar /usr/local/tomcat/
+ADD ./arex-agent-0.1.0.jar /usr/local/tomcat/
+ADD ./arex-agent-bootstrap-0.1.0.jar /usr/local/tomcat/
 
 WORKDIR /usr/local/tomcat
 EXPOSE 8080
