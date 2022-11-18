4e26c963b6be4587a555713188db8b3dc0502386
vwhb王海兵 <haibingwang@trip.com>
2022-11-18 13:34:43 +0800

    fix arex-commnuty-test config

--- community.build.sh
+++ community.build.sh
@@ -2,8 +2,8 @@ cd ../arex-community-test
 mvn clean package -DskipTests
 
 cp ../deployments/dockerfile/community.Dockerfile ./Dockerfile
-cp ./agent/arex-agent-0.*.jar ./arex-agent.jar
-cp ./agent/arex-agent-bootstrap-*.jar ./arex-agent-bootstrap.jar
+cp ../deployments/agent/arex-agent-0.*.jar ./arex-agent.jar
+cp ../deployments/agent/arex-agent-bootstrap-*.jar ./arex-agent-bootstrap.jar
 
 docker build -t arexadmin01/arex-community-test:0.0.1  .
 
