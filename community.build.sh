db2f0231bbcea2f2c795aa99f80fcbd606dda681
vwhb王海兵 <haibingwang@trip.com>
2022-11-22 10:36:15 +0800

    update  build.sh

--- community.build.sh
+++ community.build.sh
@@ -2,8 +2,10 @@ cd ../arex-community-test
 mvn clean package -DskipTests
 
 cp ../deployments/dockerfile/community.Dockerfile ./Dockerfile
-cp ../arex-agent-java/arex-agent-jar/arex-agent-0.1.0.jar ./arex-agent-0.1.0.jar
-cp ../arex-agent-java/arex-agent-jar/arex-agent-bootstrap-0.1.0.jar ./arex-agent-bootstrap-0.1.0.jar
+#cp ../arex-agent-java/arex-agent-jar/arex-agent-0.1.0.jar ./arex-agent-0.1.0.jar
+#cp ../arex-agent-java/arex-agent-jar/arex-agent-bootstrap-0.1.0.jar ./arex-agent-bootstrap-0.1.0.jar
+cp ../deployments/agent/arex-agent-0.1.0.jar ./arex-agent-0.1.0.jar
+cp ../deployments/agent/arex-agent-bootstrap-0.1.0.jar ./arex-agent-bootstrap-0.1.0.jar
 
 docker build -t arexadmin01/arex-community-test:0.0.1  .
 
