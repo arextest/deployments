497964f65595db23e2a81653ec71368540c28602
vwhb王海兵 <haibingwang@trip.com>
2023-01-05 15:26:29 +0800

    fix 0.2.6

--- arex-upload-docker.sh
+++ arex-upload-docker.sh
@@ -6,10 +6,10 @@ then
 fi
 
 echo "Tag version: $1"
-docker tag arexadmin01/arex:$1  arexadmin01/arex:latest 
-docker tag arexadmin01/arex-report:$1  arexadmin01/arex-report:latest 
-docker tag arexadmin01/arex-storage-serive:$1 arexadmin01/arex-storage-serive:latest   
-docker tag arexadmin01/arex-replay-schedule:$1 arexadmin01/arex-replay-schedule:latest 
+#docker tag arexadmin01/arex:$1  arexadmin01/arex:latest 
+#docker tag arexadmin01/arex-report:$1  arexadmin01/arex-report:latest 
+#docker tag arexadmin01/arex-storage-serive:$1 arexadmin01/arex-storage-serive:latest   
+#docker tag arexadmin01/arex-replay-schedule:$1 arexadmin01/arex-replay-schedule:latest 
 
 echo "Push version:$1"
 docker push arexadmin01/arex:$1
@@ -17,11 +17,11 @@ docker push arexadmin01/arex-report:$1
 docker push arexadmin01/arex-storage-serive:$1
 docker push arexadmin01/arex-replay-schedule:$1
 
-echo "Push latest"
-docker push arexadmin01/arex:latest
-docker push arexadmin01/arex-report:latest
-docker push arexadmin01/arex-storage-serive:latest
-docker push arexadmin01/arex-replay-schedule:latest
+#echo "Push latest"
+#docker push arexadmin01/arex:latest
+#docker push arexadmin01/arex-report:latest
+#docker push arexadmin01/arex-storage-serive:latest
+#docker push arexadmin01/arex-replay-schedule:latest
 
 echo "upload to aliyun:$1"
 docker tag arexadmin01/arex:$1  registry.cn-hangzhou.aliyuncs.com/arexadmin01/arex:$1 
