7a80682586d3b6636190af361e0b05d76c5fdae2
vwhb王海兵 <haibingwang@trip.com>
2023-01-06 16:15:19 +0800

    fix 0.2.6

--- arex-upload-docker.sh
+++ arex-upload-docker.sh
@@ -5,13 +5,13 @@ then
   exit
 fi
 
-echo "Tag version: $1"
-echo "Push version:$1"
+echo "Tag version: $1. Push Docker to docker.io and aliyuncs"
 docker push arexadmin01/arex:$1
 docker push arexadmin01/arex-report:$1
 docker push arexadmin01/arex-node:$1
 docker push arexadmin01/arex-storage-serive:$1
 docker push arexadmin01/arex-replay-schedule:$1
+docker push arexadmin01/arex-community-test:0.0.1
 
 echo "upload to aliyun:$1"
 docker tag arexadmin01/arex:$1  registry.cn-hangzhou.aliyuncs.com/arexadmin01/arex:$1 
@@ -19,10 +19,13 @@ docker tag arexadmin01/arex-node:$1  registry.cn-hangzhou.aliyuncs.com/arexadmin
 docker tag arexadmin01/arex-report:$1  registry.cn-hangzhou.aliyuncs.com/arexadmin01/arex-report:$1 
 docker tag arexadmin01/arex-storage-serive:$1 registry.cn-hangzhou.aliyuncs.com/arexadmin01/arex-storage-serive:$1   
 docker tag arexadmin01/arex-replay-schedule:$1 registry.cn-hangzhou.aliyuncs.com/arexadmin01/arex-replay-schedule:$1 
+docker tag arexadmin01/arex-community-test:0.0.1 registry.cn-hangzhou.aliyuncs.com/arexadmin01/arex-community-test:0.0.1
 
 docker push registry.cn-hangzhou.aliyuncs.com/arexadmin01/arex:$1
 docker push registry.cn-hangzhou.aliyuncs.com/arexadmin01/arex-report:$1
+docker push registry.cn-hangzhou.aliyuncs.com/arexadmin01/arex-node:$1
 docker push registry.cn-hangzhou.aliyuncs.com/arexadmin01/arex-storage-serive:$1
 docker push registry.cn-hangzhou.aliyuncs.com/arexadmin01/arex-replay-schedule:$1
+docker push registry.cn-hangzhou.aliyuncs.com/arexadmin01/arex-community-test:0.0.1
 
 
