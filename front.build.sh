1a9083332d757e26baf42c9907b3170ba6ad3a24
vwhb王海兵 <haibingwang@trip.com>
2023-05-24 11:22:23 +0800

    init for 0.2.10.1

--- front.build.sh
+++ front.build.sh
@@ -8,9 +8,9 @@ else
     exit 1
 fi
 
-printf "current AREX Version: $AREX_VERSION (From .env file)"
-cd ../arex-front-end
+printf "current AREX Version: $AREX_VERSION "
 
+cd ../arex-front-end
 cp ../deployments/dockerfile/front.Dockerfile ./Dockerfile
 docker build -t arexadmin01/arex:$AREX_VERSION  .  --build-arg http_proxy=http://ntproxy.qa.nt.ctripcorp.com:8080 --build-arg https_proxy=http://ntproxy.qa.nt.ctripcorp.com:8080 
 rm -rf ./Dockerfile
