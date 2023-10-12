3dcb8dd2d67b21e67f70a76848af146e3dc95944
haibing-wang <davidwang2008u@gmail.com>
2023-10-12 20:38:56 +0800

    test

--- front.build.sh
+++ front.build.sh
@@ -10,7 +10,7 @@ fi
 
 printf "current AREX Version: $AREX_VERSION "
 
-cd ../arex-front-end
+cd ../arex
 cp ../deployments/dockerfile/front.Dockerfile ./Dockerfile
 docker build -t arexadmin01/arex:$AREX_VERSION  .  --build-arg http_proxy=http://ntproxy.qa.nt.ctripcorp.com:8080 --build-arg https_proxy=http://ntproxy.qa.nt.ctripcorp.com:8080 
 rm -rf ./Dockerfile
